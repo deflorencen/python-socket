2e822294db100caf975a952f690399be180455ff
deflorencen <yevgeniysarancha@gmail.com>
2023-04-08 13:25:03 +0200

    - version 1.1

--- client.py
+++ client.py
@@ -4,115 +4,110 @@ import command
 import random
 from argon2 import PasswordHasher
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect(("127.0.0.1", 12345))
+    client.connect(('127.0.0.1', 50001))
 
-        while True:
-            option = input("\n\t#######################################"
-                           "\n\t1: Create user"
-                           "\n\t2: Check account status"
-                           "\n\t3: Payment on account"
-                           "\n\t4: Make the payment"
-                           "\n\t5: Transfer money to another account"
-                           "\n\t#######################################"
-                           "\n"
-                           "\nCHOOSE YOU OPTION: ")
+    while True:
+        option = input("\n\t#######################################"
+                       "\n\t1: Create user"
+                       "\n\t2: Check account status"
+                       "\n\t3: Payment on account"
+                       "\n\t4: Make the payment"
+                       "\n\t5: Transfer money to another account"
+                       "\n\t#######################################"
+                       "\n"
+                       "\nCHOOSE YOU OPTION: ")
 
-            if (option == "1"):
-                command.send_request_to_server(client, "1")
+        if (option == "1"):
+            command.send_request_to_server(client, "1")
 
-                print(command.get_message_from_server(client))
+            print(command.get_message_from_server(client))
 
-                fname = input("First name: ")
-                lname = input("Last name: ")
-                pesel = input("PESEL: ")
-                password = input("Password: ")
-                hash_password = command.hash_password(password)
-                id = 0
+            fname = input("First name: ")
+            lname = input("Last name: ")
+            pesel = input("PESEL: ")
+            password = input("Password: ")
+            hash_password = command.hash_password(password)
+            id = 0
 
-                user = {"First name": fname, "Last name": lname, "KEY": id, "PESEL": pesel, "Password": hash_password,
-                        "Balance": 0}
-                send_data = json.dumps(user)
-                command.send_request_to_server(client, send_data)
+            user = {"First name": fname, "Last name": lname, "KEY": id, "PESEL": pesel, "Password": hash_password,
+                    "Balance": 0}
+            send_data = json.dumps(user)
+            command.send_request_to_server(client, send_data)
 
-                server_message = command.get_message_from_server(client)
-                print(server_message)
+            server_message = command.get_message_from_server(client)
+            print(server_message)
 
-            elif (option == "2"):
-                command.send_request_to_server(client, "2")
+        elif (option == "2"):
+            command.send_request_to_server(client, "2")
 
-                print(command.get_message_from_server(client))
+            print(command.get_message_from_server(client))
 
-                fname = input("First name: ")
-                lname = input("Last name: ")
+            fname = input("First name: ")
+            lname = input("Last name: ")
 
-                user = {"First name": fname, "Last name": lname}
-                data_to_find = json.dumps(user)
-                command.send_request_to_server(client, data_to_find)
+            user = {"First name": fname, "Last name": lname}
+            data_to_find = json.dumps(user)
+            command.send_request_to_server(client, data_to_find)
 
-                server_message = command.get_message_from_server(client)
-                print(server_message)
+            server_message = command.get_message_from_server(client)
+            print(server_message)
 
-            elif (option == "3"):
-                command.send_request_to_server(client, "3")
+        elif (option == "3"):
+            command.send_request_to_server(client, "3")
 
-                print(command.get_message_from_server(client))
+            print(command.get_message_from_server(client))
 
-                fname = input("First name:")
-                lname = input("Last name:")
-                balance = input("Sum to payment:")
-                password = input("Password:")
-                user = {"First name": fname, "Last name": lname, "Balance": balance, "Password": password}
+            fname = input("First name:")
+            lname = input("Last name:")
+            balance = input("Sum to payment:")
+            password = input("Password:")
+            user = {"First name": fname, "Last name": lname, "Balance": balance, "Password": password}
 
-                data_to_changing = json.dumps(user)
-                command.send_request_to_server(client, data_to_changing)
+            data_to_changing = json.dumps(user)
+            command.send_request_to_server(client, data_to_changing)
 
-                server_message = command.get_message_from_server(client)
-                print(server_message)
+            server_message = command.get_message_from_server(client)
+            print(server_message)
 
-            elif (option == "4"):
-                command.send_request_to_server(client, "4")
+        elif (option == "4"):
+            command.send_request_to_server(client, "4")
 
-                print(command.get_message_from_server(client))
+            print(command.get_message_from_server(client))
 
-                fname = input("First name:")
-                lname = input("Last name:")
-                balance = input("Sum to payment:")
-                password = input("Password:")
-                user = {"First name": fname, "Last name": lname, "Balance": balance, "Password": password}
+            fname = input("First name:")
+            lname = input("Last name:")
+            balance = input("Sum to payment:")
+            password = input("Password:")
+            user = {"First name": fname, "Last name": lname, "Balance": balance, "Password": password}
 
-                data_to_changing = json.dumps(user)
-                command.send_request_to_server(client, data_to_changing)
+            data_to_changing = json.dumps(user)
+            command.send_request_to_server(client, data_to_changing)
 
-                server_message = command.get_message_from_server(client)
-                print(server_message)
+            server_message = command.get_message_from_server(client)
+            print(server_message)
 
-            elif (option == "5"):
-                command.send_request_to_server(client, "5")
+        elif (option == "5"):
+            command.send_request_to_server(client, "5")
 
-                print(command.get_message_from_server(client))
+            print(command.get_message_from_server(client))
 
-                owner_fname = input("First name:")
-                owner_lname = input("Last name:")
-                money_to_transfer = input("Money to transfer: ")
-                password = input("Password:")
-                print("Transfer to..")
-                fname_t = input("First name:")
-                lname_t = input("Last name:")
-                users = {"First name": owner_fname, "Last name": owner_lname, "Password": password, "Money to transfe": money_to_transfer,}
+            owner_fname = input("First name:")
+            owner_lname = input("Last name:")
+            money_to_transfer = input("Money to transfer: ")
+            password = input("Password:")
+            print("Transfer to..")
+            fname_t = input("First name:")
+            lname_t = input("Last name:")
+            users = {"First name": owner_fname, "Last name": owner_lname, "Password": password,
+                     "Money to transfe": money_to_transfer, }
 
-                data_to_changing = json.dumps(users)
-                command.send_request_to_server(client, data_to_changing)
+            data_to_changing = json.dumps(users)
+            command.send_request_to_server(client, data_to_changing)
 
-
-
-
-            elif(option == "q"):
-                command.send_request_to_server(client, "q")
-                break
-
-
-
+        elif (option == "q"):
+            command.send_request_to_server(client, "q")
+            break
 except ConnectionRefusedError:
     print("NOT CONNECTION..")
--- server.py
+++ server.py
@@ -2,34 +2,42 @@ import socket
 import threading
 import command
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind(("127.0.0.1", 12345))
-    server.listen(0)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 50001))
 
-    print("\n\tSERVER IS WORKING...")
+print("\n\tSERVER IS WORKING...")
+
+def handle_client(conn, addr):
     while True:
-        server, address = server.accept()
+        option = command.get_message_from_client(conn)
 
-        with server:
+        if (option == "1"):
+            command.create_user(conn)
 
-            while True:
-                option = server.recv(1024).decode("utf-8")
+        elif (option == "2"):
+            command.check_account_status(conn)
 
-                if (option == "1"):
-                    command.create_user(server)
+        elif (option == "3"):
+            command.payment_on_account(conn)
 
-                elif (option == "2"):
-                    command.check_account_status(server)
+        elif (option == "4"):
+            command.make_the_payment(conn)
 
-                elif (option == "3"):
-                    command.payment_on_account(server)
+        elif (option == "5"):
+            command.transfer_money_to_account(conn)
 
-                elif (option == "4"):
-                    command.make_the_payment(server)
+        if (option == "q"):
+            print('Server is stoped')
+            break
 
-                elif (option == "5"):
-                    command.transfer_money_to_account(server)
+def start():
+    server.listen(0)
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(
+            f'{addr} is connected\nCount of active conection {threading.active_count() - 1}\n')
 
-                if (option == "q"):
-                    print('Server is stoped')
-                    break
+print(f'server is starting...')
+start()
